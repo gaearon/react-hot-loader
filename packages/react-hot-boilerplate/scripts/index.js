d9d75d070f4888191ce300e6774a36b5b2759e02
Dan Abramov <dan.abramov@gmail.com>
2014-08-23 22:55:32 +0400

    Add code to enable React devtools

--- packages/react-hot-boilerplate/scripts/index.js
+++ packages/react-hot-boilerplate/scripts/index.js
@@ -3,5 +3,10 @@
 
 var React = require('react'),
     App = require('./App');
+    
+if ('production' !== process.env.NODE_ENV) {
+  // Enable React devtools
+  window['React'] = React;
+}
 
 React.renderComponent(<App />, document.body);
