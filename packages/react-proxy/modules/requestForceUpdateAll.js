e5a718a1c26e4f06cee5e77e0c845eed5dde2bc4
Dan Abramov <dan.abramov@gmail.com>
2014-12-12 07:07:46 +0100

    Pass ReactMount inside

--- packages/react-proxy/modules/requestForceUpdateAll.js
+++ packages/react-proxy/modules/requestForceUpdateAll.js
@@ -2,26 +2,28 @@ var deepForceUpdate = require('./deepForceUpdate');
 
 var isRequestPending = false;
 
-/**
- * Forces deep re-render of all mounted React components.
- * Hat's off to Omar Skalli (@Chetane) for suggesting this approach:
- * https://gist.github.com/Chetane/9a230a9fdcdca21a4e29
- */
-function forceUpdateAll(ReactMount) {
-  isRequestPending = false;
+module.exports = function requestForceUpdateAll(ReactMount) {
+  if (isRequestPending) {
+    return;
+  }
 
-  var rootInstances = ReactMount._instancesByReactRootID || ReactMount._instancesByContainerID,
-      rootInstance;
+  /**
+   * Forces deep re-render of all mounted React components.
+   * Hat's off to Omar Skalli (@Chetane) for suggesting this approach:
+   * https://gist.github.com/Chetane/9a230a9fdcdca21a4e29
+   */
+  function forceUpdateAll() {
+    isRequestPending = false;
 
-  for (var key in rootInstances) {
-    if (rootInstances.hasOwnProperty(key)) {
-      deepForceUpdate(rootInstances[key]);
+    var rootInstances = ReactMount._instancesByReactRootID || ReactMount._instancesByContainerID,
+        rootInstance;
+
+    for (var key in rootInstances) {
+      if (rootInstances.hasOwnProperty(key)) {
+        deepForceUpdate(rootInstances[key]);
+      }
     }
   }
-}
 
-module.exports = function requestForceUpdateAll() {
-  if (!isRequestPending) {
-    setTimeout(forceUpdateAll, 0);
-  }
+  setTimeout(forceUpdateAll);
 };
