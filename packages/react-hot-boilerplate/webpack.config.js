c2364edd609b52217729439196ab15ecdad7fd22
Dan Abramov <dan.abramov@gmail.com>
2015-01-01 19:25:42 +0100

    Exclude node_modules from loaders

--- packages/react-hot-boilerplate/webpack.config.js
+++ packages/react-hot-boilerplate/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, loaders: ['react-hot', 'jsx?harmony'] },
+      { test: /\.js$/, loaders: ['react-hot', 'jsx?harmony'], exclude: /node_modules/ },
     ]
   }
 };
