62e08156b8ae84e78ae5124c8f206f03dcc7a5c2
Dan Abramov <dan.abramov@gmail.com>
2015-02-02 06:20:25 +0300

    Revert "Use 0.0.0.0 instead of localhost to allow access on same network"
    This reverts commit 66d402339906858e0169e844313a6cf5665e75eb.
    
    Reverted because it doesn't work on Windows, and iOS devices don't seem to pick up hot updates for some reason anyway.

--- packages/react-hot-boilerplate/webpack.config.js
+++ packages/react-hot-boilerplate/webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?http://0.0.0.0:3000',
+    'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
     './scripts/index'
   ],
