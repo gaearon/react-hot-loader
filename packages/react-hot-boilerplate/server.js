c9d9b939848c2872c15e6d84ccc83dba706ea123
Dan Abramov <dan.abramov@gmail.com>
2015-02-20 20:21:49 +0300

    Add historyApiFallback so it works with router out of the box

--- packages/react-hot-boilerplate/server.js
+++ packages/react-hot-boilerplate/server.js
@@ -4,7 +4,8 @@ var config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
-  hot: true
+  hot: true,
+  historyApiFallback: true
 }).listen(3000, 'localhost', function (err, result) {
   if (err) {
     console.log(err);
