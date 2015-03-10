1cbfa5ca7c39bb4d355c0cd0cb81d6e55f971f3d
Dan Abramov <dan.abramov@gmail.com>
2015-03-10 03:48:45 +0300

    Re-add file with correct casing

---
+++ packages/react-hot-boilerplate/scripts/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = React.createClass({
+  render() {
+    return (
+      <h1>Hello, world.</h1>
+    );
+  }
+});
+
+export default App;
--- packages/react-hot-boilerplate/scripts/index.js
+++ packages/react-hot-boilerplate/scripts/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
-import App from './app';
+import App from './App';
 
 React.render(<App />, document.getElementById('root'));
