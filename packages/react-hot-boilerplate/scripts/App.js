3c802b85a213c85ace4a89515e1477b49815b201
Dan Abramov <dan.abramov@gmail.com>
2015-03-11 06:13:42 +0300

    Update to React 0.13

--- packages/react-hot-boilerplate/scripts/App.js
+++ packages/react-hot-boilerplate/scripts/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const App = React.createClass({
+export default class App extends React.Component {
   render() {
     return (
       <h1>Hello, world.</h1>
     );
   }
-});
-
-export default App;
+}
