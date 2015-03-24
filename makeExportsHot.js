e857fb7f11c9192856bdaa05d3f1c10638bd5e45
Dan Abramov <dan.abramov@gmail.com>
2015-03-24 14:11:57 +0300

    Refactor for readability

--- makeExportsHot.js
+++ makeExportsHot.js
@@ -5,28 +5,39 @@ var isReactClassish = require('./isReactClassish'),
 
 function makeExportsHot(m, React) {
   if (isReactElementish(m.exports)) {
+    // React elements are never valid React classes
     return false;
   }
-  
+
   var freshExports = m.exports,
-      exportsIsReactClass = isReactClassish(m.exports, React),
+      exportsReactClass = isReactClassish(m.exports, React),
       foundReactClasses = false;
 
-  if (exportsIsReactClass) {
+  if (exportsReactClass) {
     m.exports = m.makeHot(m.exports, '__MODULE_EXPORTS');
     foundReactClasses = true;
   }
 
   for (var key in m.exports) {
-    if ((!exportsIsReactClass || key !== "type") &&
-        Object.prototype.hasOwnProperty.call(freshExports, key) &&
-        isReactClassish(freshExports[key], React)) {
-      if (Object.getOwnPropertyDescriptor(m.exports, key).writable) {
-        m.exports[key] = m.makeHot(freshExports[key], '__MODULE_EXPORTS_' + key);
-        foundReactClasses = true;
-      } else {
-        console.warn("Can't make class " + key + " hot reloadable due to being read-only. You can exclude files or directories (for example, /node_modules/) using 'exclude' option in loader configuration.");
-      }
+    if (!Object.prototype.hasOwnProperty.call(freshExports, key)) {
+      continue;
+    }
+
+    if (exportsReactClass && key === 'type') {
+      // React 0.12 also puts classes under `type` property for compat.
+      // Skip to avoid updating twice.
+      continue;
+    }
+
+    if (!isReactClassish(freshExports[key], React)) {
+      continue;
+    }
+
+    if (Object.getOwnPropertyDescriptor(m.exports, key).writable) {
+      m.exports[key] = m.makeHot(freshExports[key], '__MODULE_EXPORTS_' + key);
+      foundReactClasses = true;
+    } else {
+      console.warn("Can't make class " + key + " hot reloadable due to being read-only. You can exclude files or directories (for example, /node_modules/) using 'exclude' option in loader configuration.");
     }
   }
 
