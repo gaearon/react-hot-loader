64dd55a0cca4b4a117b087053601489a2f9ddaf0
Dan Abramov <dan.abramov@gmail.com>
2015-08-22 23:24:59 +0300

    Silently ignore when export can't be read. Fixes #114

--- makeExportsHot.js
+++ makeExportsHot.js
@@ -29,12 +29,19 @@ function makeExportsHot(m, React) {
       continue;
     }
 
-    if (!isReactClassish(freshExports[key], React)) {
+    var value;
+    try {
+      value = freshExports[key];
+    } catch (err) {
+      continue;
+    }
+
+    if (!isReactClassish(value, React)) {
       continue;
     }
 
     if (Object.getOwnPropertyDescriptor(m.exports, key).writable) {
-      m.exports[key] = m.makeHot(freshExports[key], '__MODULE_EXPORTS_' + key);
+      m.exports[key] = m.makeHot(value, '__MODULE_EXPORTS_' + key);
       foundReactClasses = true;
     } else {
       console.warn("Can't make class " + key + " hot reloadable due to being read-only. To fix this you can try two solutions. First, you can exclude files or directories (for example, /node_modules/) using 'exclude' option in loader configuration. Second, if you are using Babel, you can enable loose mode for `es6.modules` using the 'loose' option. See: http://babeljs.io/docs/advanced/loose/ and http://babeljs.io/docs/usage/options/");
