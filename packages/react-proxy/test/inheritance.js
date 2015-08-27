8bde60f069d279f2f4013d80dd9bb0a1fcfc0506
Dan Abramov <dan.abramov@gmail.com>
2015-08-28 00:30:43 +0300

    Add a fancier test

--- packages/react-proxy/test/inheritance.js
+++ packages/react-proxy/test/inheritance.js
@@ -125,5 +125,61 @@ describe('inheritance', () => {
       derivedProxy.update(Derived2).forEach(forceUpdate);
       expect(renderer.getRenderOutput().props.children).toEqual(4200);
     });
+
+    it('replaces any method with proxied base, middle and derived', () => {
+      const baseProxy = createProxy(Base1);
+      const BaseProxy = baseProxy.get();
+
+      class Middle1 extends BaseProxy {
+        actuallyRender() {
+          return <span>{super.getX() * 10}</span>;
+        }
+      }
+
+      class Middle2 extends BaseProxy {
+        actuallyRender() {
+          return <span>{super.getX() * 100}</span>;
+        }
+      }
+
+      const middleProxy = createProxy(Middle1);
+      const MiddleProxy = middleProxy.get();
+
+      class Derived1 extends MiddleProxy {
+        render() {
+          return <span>{super.render().props.children + ' lol'}</span>;
+        }
+      }
+
+      class Derived2 extends MiddleProxy {
+        render() {
+          return <span>{super.render().props.children + ' nice'}</span>;
+        }
+      }
+
+      const derivedProxy = createProxy(Derived1);
+      const DerivedProxy = derivedProxy.get();
+
+      const instance = renderer.render(<DerivedProxy />);
+      expect(renderer.getRenderOutput().props.children).toEqual('420 lol');
+
+      baseProxy.update(Base2).forEach(forceUpdate);
+      expect(renderer.getRenderOutput().props.children).toEqual('430 lol');
+
+      middleProxy.update(Middle2).forEach(forceUpdate);
+      expect(renderer.getRenderOutput().props.children).toEqual('4300 lol');
+
+      derivedProxy.update(Derived2).forEach(forceUpdate);
+      expect(renderer.getRenderOutput().props.children).toEqual('4300 nice');
+
+      derivedProxy.update(Derived1).forEach(forceUpdate);
+      expect(renderer.getRenderOutput().props.children).toEqual('4300 lol');
+
+      middleProxy.update(Middle1).forEach(forceUpdate);
+      expect(renderer.getRenderOutput().props.children).toEqual('430 lol');
+
+      baseProxy.update(Base1).forEach(forceUpdate);
+      expect(renderer.getRenderOutput().props.children).toEqual('420 lol');
+    });
   });
 });
