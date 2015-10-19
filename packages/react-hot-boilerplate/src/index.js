5e38f2bb899b5df494a7ed85307fd7387fd30c63
Václav Šír <vaclav.sir@gmail.com>
2015-10-19 09:14:09 +0200

    Update dependencies; replace React.render with ReactDOM.render

--- packages/react-hot-boilerplate/src/index.js
+++ packages/react-hot-boilerplate/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-React.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
