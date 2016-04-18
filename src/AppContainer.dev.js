47042fbb8529c826fe90940d2f093b3a97f444a5
Dan Abramov <dan.abramov@gmail.com>
2016-04-18 11:35:54 +0100

    Add a few friendly errors

--- src/AppContainer.dev.js
+++ src/AppContainer.dev.js
@@ -3,30 +3,33 @@ const deepForceUpdate = require('react-deep-force-update');
 const Redbox = require('redbox-react');
 const { Component } = React;
 
-function ensureCreateElementIsPatched() {
-  const A = () => {};
-  A.__source = { fileName: 'fake', localName: 'fake' }
-  const B = () => {};
-  B.__source = { fileName: 'fake', localName: 'fake' }
-
-  if (<A />.type !== <B />.type) {
-    console.error(
-      'React Hot Loader: It appears that you did not run ' +
-      '"react-hot-loader/patch". Make sure that this module ' +
-      'runs before any other code. For example, if you use Webpack, ' +
-      'you can add "react-hot-loader/patch" as the very first item to the ' +
-      '"entry" array in its config. Alternatively, you can add ' +
-      'require("react-hot-loader/patch") as the very first line ' +
-      'in the application code, before any other imports.'
-    );
-  }
+let wasCreateElementPatched = false;
+const A = () => {};
+A.__source = { fileName: 'fake', localName: 'fake' }
+const B = () => {};
+B.__source = { fileName: 'fake', localName: 'fake' }
+if (<A />.type === <B />.type) {
+  wasCreateElementPatched = true;
 }
-ensureCreateElementIsPatched();
 
 class AppContainer extends Component {
   constructor(props) {
     super(props);
     this.state = { error: null };
+  }
+
+  componentDidMount() {
+    if (!wasCreateElementPatched) {
+      console.error(
+        'React Hot Loader: It appears that "react-hot-loader/patch" ' +
+        'did not run immediately before the app started. Make sure that it ' +
+        'runs before any other code. For example, if you use Webpack, ' +
+        'you can add "react-hot-loader/patch" as the very first item to the ' +
+        '"entry" array in its config. Alternatively, you can add ' +
+        'require("react-hot-loader/patch") as the very first line ' +
+        'in the application code, before any other imports.'
+      );
+    }
   }
 
   componentWillReceiveProps(nextProps) {
--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
 const AppContainer = require('./AppContainer');
+
+module.exports = function warnAboutIncorrectUsage() {
+  if (this && this.callback) {
+    throw new Error(
+      'React Hot Loader is now more than a Webpack loader. ' +
+      'Make sure that in your Webpack "loaders" configuration ' +
+      'you use "react-hot-loader/webpack" rather than just ' +
+      '"react-hot-loader". Alternatively, if you already use Babel, ' +
+      'you may remove it from "loaders" completely, and instead add ' +
+      '"react-hot-loader/babel" to "plugins" in your .babelrc.'
+    );
+  } else {
+    throw new Error(
+      'React Hot Loader does not have a default export. ' +
+      'If you use the import statement, make sure to include ' +
+      'curly braces: import { AppContainer } from "react-hot-loader". ' +
+      'If you use CommonJS, make sure to read the named export: ' +
+      'require("react-hot-loader").AppContainer.'
+    );
+  }
+}
+
 module.exports.AppContainer = AppContainer;
