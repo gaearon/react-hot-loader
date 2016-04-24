cdd8c6d598ae75bf657a6da5b12e10712f3056b8
Alexander Christiansson <alexander.christiansson@gmail.com>
2016-04-25 01:24:44 +0200

    Add tests for react router hack

---
+++ test/hackRouter/index.js
@@ -0,0 +1,126 @@
+import hackRouter from '../../src/hackRouter';
+import expect, { createSpy } from 'expect';
+
+const forceUpdateSpy = createSpy();
+
+const Router = {
+  displayName: 'Router',
+};
+
+describe('hackRouter', () => {
+  beforeEach(() => {
+    forceUpdateSpy.reset();
+  });
+
+  describe('when routes are passed in as children', () => {
+    /* Equivalent to:
+    <Router>
+      <Route component={function a() {}} />
+      <Route component={function b() {}} />
+    </Router> */
+
+    const props = {
+      children: [
+        { props: { component: function a() {} } },
+        { props: { component: function b() {} } },
+      ],
+    };
+
+    it('calls force update on each route\'s component', () => {
+      hackRouter(Router, props, forceUpdateSpy);
+      expect(forceUpdateSpy).toHaveBeenCalledWith(props.children[0].props.component);
+      expect(forceUpdateSpy).toHaveBeenCalledWith(props.children[1].props.component);
+    });
+
+    describe('and the top component is not a router', () => {
+      it('does not call force update on any component', () => {
+        const NotRouter = { displayName: 'DefinitelyNotARouter' };
+        hackRouter(NotRouter, props, forceUpdateSpy);
+        expect(forceUpdateSpy).toNotHaveBeenCalled();
+      });
+    });
+  });
+
+  describe('when routes are passed in as children of children', () => {
+    /* Equivalent to:
+    <Router>
+      <Route component={function a() {}}>
+        <Route component={function b() {}} />
+      </Route>
+    </Router> */
+
+    const props = {
+      children: [
+        {
+          props: {
+            component: function a() {},
+            children: [
+              { props: { component: function b() {} } },
+            ],
+          },
+        },
+      ],
+    };
+
+    it('calls force update on each route\'s component', () => {
+      hackRouter(Router, props, forceUpdateSpy);
+      expect(forceUpdateSpy).toHaveBeenCalledWith(props.children[0].props.component);
+      expect(forceUpdateSpy).toHaveBeenCalledWith(
+        props.children[0].props.children[0].props.component
+      );
+    });
+  });
+
+  describe('when routes are passed in using the `routes` prop', () => {
+    const props = {
+      routes: [
+        { component: function a() {} },
+        { component: function b() {} },
+      ],
+    };
+
+    it('calls force update on each route\'s components', () => {
+      hackRouter(Router, props, forceUpdateSpy);
+      expect(forceUpdateSpy).toHaveBeenCalledWith(props.routes[0].component);
+      expect(forceUpdateSpy).toHaveBeenCalledWith(props.routes[1].component);
+    });
+  });
+
+  describe('when routes are passed in using the `routes` and `childRoutes` props', () => {
+    const props = {
+      routes: [
+        {
+          component: function a() {},
+          childRoutes: [
+            { component: function b() {} },
+          ],
+        },
+      ],
+    };
+
+    it('calls force update on each route\'s components', () => {
+      hackRouter(Router, props, forceUpdateSpy);
+      expect(forceUpdateSpy).toHaveBeenCalledWith(props.routes[0].component);
+      expect(forceUpdateSpy).toHaveBeenCalledWith(props.routes[0].childRoutes[0].component);
+    });
+  });
+
+  describe('when there are multiple components on one route', () => {
+    const props = {
+      routes: [
+        {
+          components: {
+            a: function a() {},
+            b: function b() {},
+          },
+        },
+      ],
+    };
+
+    it('calls force update on each component', () => {
+      hackRouter(Router, props, forceUpdateSpy);
+      expect(forceUpdateSpy).toHaveBeenCalledWith(props.routes[0].components.a);
+      expect(forceUpdateSpy).toHaveBeenCalledWith(props.routes[0].components.b);
+    });
+  });
+});
--- test/index.js
+++ test/index.js
@@ -1,1 +1,2 @@
 import './babel';
+import './hackRouter';
