613c428cc84bdaa9cd766aac57e0729f34153f7e
Nuno Campos <nuno@crowdprocess.com>
2016-05-01 18:52:31 +0100

    tests for HOC-wrapped root

--- test/AppContainer/AppContainer.dev.js
+++ test/AppContainer/AppContainer.dev.js
@@ -186,5 +186,104 @@ describe('<AppContainer />', () => {
         expect(wrapper.text()).toBe('new render + old state')
       })
     })
+
+    describe('with HOC-wrapped root', () => {
+      const mapProps = require('recompose').mapProps
+      it('renders it', () => {
+        const spy = createSpy()
+        class App extends React.Component {
+          render() {
+            spy()
+            return <div>hey</div>
+          }
+        }
+        tag(App, 'App')
+
+        const Enhanced = mapProps(props => ({ n: props.n * 5 }))(App)
+        tag(Enhanced, 'Enhanced')
+
+        const wrapper = mount(<AppContainer><Enhanced n={3} /></AppContainer>)
+        expect(wrapper.find('App').length).toBe(1)
+        expect(wrapper.contains(<div>hey</div>)).toBe(true)
+        expect(wrapper.find('App').prop('n')).toBe(15)
+        expect(spy.calls.length).toBe(1)
+      })
+
+      it('force updates the tree on receiving new children', () => {
+        const spy = createSpy()
+        class App extends React.Component {
+          render() {
+            spy()
+            return <div>hey</div>
+          }
+        }
+        tag(App, 'App')
+
+        const Enhanced = mapProps(props => ({ n: props.n * 5 }))(App)
+        tag(Enhanced, 'Enhanced')
+
+        const wrapper = mount(<AppContainer><Enhanced n={3} /></AppContainer>)
+        expect(spy.calls.length).toBe(1)
+
+        {
+          class App extends React.Component {
+            render() {
+              spy()
+              return <div>ho</div>
+            }
+          }
+          tag(App, 'App')
+
+          const Enhanced = mapProps(props => ({ n: props.n * 5 }))(App)
+          tag(Enhanced, 'Enhanced')
+          wrapper.setProps({children: <Enhanced n={3} />})
+        }
+
+        expect(spy.calls.length).toBe(2)
+        expect(wrapper.contains(<div>ho</div>)).toBe(true)
+      })
+
+      it('hot-reloads without losing state', () => {
+        class App extends Component {
+          componentWillMount() {
+            this.state = 'old'
+          }
+
+          shouldComponentUpdate() { return false }
+
+          render() {
+            return <div>old render + {this.state} state + {this.props.n}</div>
+          }
+        }
+        tag(App, 'App')
+
+        const Enhanced = mapProps(props => ({ n: props.n * 5 }))(App)
+        tag(Enhanced, 'Enhanced')
+
+        const wrapper = mount(<AppContainer><Enhanced n={3} /></AppContainer>)
+        expect(wrapper.text()).toBe('old render + old state + 15')
+
+        {
+          class App extends Component {
+            componentWillMount() {
+              this.state = 'new'
+            }
+
+            shouldComponentUpdate() { return false }
+
+            render() {
+              return <div>new render + {this.state} state + {this.props.n}</div>
+            }
+          }
+          tag(App, 'App')
+
+          const Enhanced = mapProps(props => ({ n: props.n * 5 }))(App)
+          tag(Enhanced, 'Enhanced')
+          wrapper.setProps({children: <Enhanced n={4} />})
+        }
+
+        expect(wrapper.text()).toBe('new render + old state + 20')
+      })
+    })
   })
 })
