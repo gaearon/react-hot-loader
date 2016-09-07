669b9a575d2f4bd272bcf280583bbdf946e44315
Cale Newman <newman.cale@gmail.com>
2016-09-06 19:53:59 -0500

    Remove unneeded transforms from babel plugin tests
    We only care about what the plugin does, and not about how Babel
    transpiles ES2015/class properties transforms. This makes the fixtures
    more immediately understandable.

--- test/babel/fixtures/class-properties/block-body/expected.js
+++ test/babel/fixtures/class-properties/block-body/expected.js
@@ -1,30 +1,13 @@
-"use strict";
+var _this = this;
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+class Foo {
+  bar = (...params) => _this.__bar__REACT_HOT_LOADER__(...params);
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Foo = function () {
-  function Foo() {
-    var _this = this;
-
-    _classCallCheck(this, Foo);
-
-    this.bar = function () {
-      return _this.__bar__REACT_HOT_LOADER__.apply(_this, arguments);
-    };
+  __bar__REACT_HOT_LOADER__(a, b) {
+    return a(b);
   }
 
-  _createClass(Foo, [{
-    key: "__bar__REACT_HOT_LOADER__",
-    value: function __bar__REACT_HOT_LOADER__(a, b) {
-      return a(b);
-    }
-  }]);
-
-  return Foo;
-}();
-
+}
 ;
 
 (function () {
