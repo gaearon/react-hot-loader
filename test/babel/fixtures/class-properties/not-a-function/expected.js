4f346653ac8693fdb0db89625411d3e6d33025a0
Nuno Campos <nuno.campos@me.com>
2016-09-26 09:40:41 +0100

    more passing tests for class properties transform

---
+++ test/babel/fixtures/class-properties/not-a-function/expected.js
@@ -0,0 +1,14 @@
+class Foo {
+  bar = 2;
+}
+;
+
+var _temp = function () {
+  if (typeof __REACT_HOT_LOADER__ === 'undefined') {
+    return;
+  }
+
+  __REACT_HOT_LOADER__.register(Foo, "Foo", __FILENAME__);
+}();
+
+;
---
+++ test/babel/fixtures/class-properties/not-an-arrow-function/actual.js
@@ -0,0 +1,5 @@
+class Foo {
+  bar = function (a, b) {
+    return a(b);
+  };
+}
