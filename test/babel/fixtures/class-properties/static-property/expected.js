12f3fdd0a32ef263fd8f28807a2111ef1a1af465
Nuno Campos <nuno.campos@me.com>
2016-09-26 09:33:52 +0100

    added failing test for static class properties

---
+++ test/babel/fixtures/class-properties/static-property/expected.js
@@ -0,0 +1,16 @@
+class Foo {
+  static bar = (a, b) => {
+    return a(b);
+  };
+}
+;
+
+var _temp = function () {
+  if (typeof __REACT_HOT_LOADER__ === 'undefined') {
+    return;
+  }
+
+  __REACT_HOT_LOADER__.register(Foo, "Foo", __FILENAME__);
+}();
+
+;
