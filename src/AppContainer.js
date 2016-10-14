01fc6c8abf4c68c1eb00203dc1ee0b925496c93f
Cale Newman <newman.cale@gmail.com>
2016-10-13 20:40:34 -0500

    Use production patch/AppContainer if no module.hot
    Prevents cases where NODE_ENV=test imports the development AppContainer
    and patching code. We can leave the NODE_ENV=production check in for
    manually opting out. Closes #396.

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-if (!module.hot && process.env.NODE_ENV === 'production') {
+if (!module.hot || process.env.NODE_ENV === 'production') {
   module.exports = require('./AppContainer.prod');
 } else {
   module.exports = require('./AppContainer.dev');
