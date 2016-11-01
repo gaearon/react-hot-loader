8f758ee5c3c12e3f6501cdea79671695a816bd54
Adam Beck <AdamB.Infinity@gmail.com>
2016-11-01 16:31:38 -0400

    Use webpack-dev-middleware (#68)
    * Use webpack-dev-middleware
    
    * Use webpack-dev-middleware

--- packages/react-hot-boilerplate/server.js
+++ packages/react-hot-boilerplate/server.js
@@ -1,15 +1,25 @@
+var path = require('path');
 var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
+var express = require('express');
 var config = require('./webpack.config');
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+var app = express();
+var compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(3000, function(err) {
   if (err) {
-    return console.log(err);
+    return console.error(err);
   }
 
   console.log('Listening at http://localhost:3000/');
-});
+})
