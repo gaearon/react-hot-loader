1736ab6930c72b7bd3962d3bd486f7f52c5a8a8b
Dan Abramov <dan.abramov@gmail.com>
2017-08-09 01:00:33 +0100

    Support Fiber

--- packages/react-deep-force-update/src/index.js
+++ packages/react-deep-force-update/src/index.js
@@ -1,3 +1,7 @@
+// Constant to identify a React Component. It's been extracted from ReactTypeOfWork
+// (https://github.com/facebook/react/blob/master/src/shared/ReactTypeOfWork.js#L20)
+const ReactClassComponent = 2;
+
 function traverseRenderedChildren(internalInstance, callback, argument) {
   callback(internalInstance, argument);
 
@@ -39,8 +43,45 @@ function forceUpdateIfPending(internalInstance) {
   }
 }
 
-export default function deepForceUpdate(instance) {
+function deepForceUpdateStack(instance) {
   const internalInstance = instance._reactInternalInstance;
   traverseRenderedChildren(internalInstance, setPendingForceUpdate);
   traverseRenderedChildren(internalInstance, forceUpdateIfPending);
+}
+
+export default function deepForceUpdate(instance) {
+  const root = instance._reactInternalInstance;
+  if (typeof root.tag !== 'number') {
+    // Traverse stack-based React tree.
+    return deepForceUpdateStack(instance);
+  }
+
+  let node = root;
+  while (true) {
+    if (node.tag === ReactClassComponent) {
+      const publicInstance = node.stateNode;
+      const { updater } = publicInstance;
+      if (typeof publicInstance.forceUpdate === 'function') {
+        publicInstance.forceUpdate();
+      } else if (updater && typeof updater.enqueueForceUpdate === 'function') {
+        updater.enqueueForceUpdate(publicInstance);
+      }
+    }
+    if (node.child) {
+      node.child.return = node;
+      node = node.child;
+      continue;
+    }
+    if (node === root) {
+      return undefined;
+    }
+    while (!node.sibling) {
+      if (!node.return || node.return === root) {
+        return undefined;
+      }
+      node = node.return;
+    }
+    node.sibling.return = node.return;
+    node = node.sibling;
+  }
 }
