80fb0572ed064c9b02277e621c605845f8c60039
Anton Korzunov <akorzunov@atlassian.com>
2017-12-07 00:16:23 +1100

    store ID inside proxy

--- packages/react-stand-in/src/symbols.js
+++ packages/react-stand-in/src/symbols.js
@@ -1,3 +1,4 @@
 export const PREFIX = '__reactstandin__'
+export const PROXY_KEY = `${PREFIX}key`
 export const GENERATION = `${PREFIX}proxyGeneration`
 export const REGENERATE_METHOD = `${PREFIX}regenerateByEval`
