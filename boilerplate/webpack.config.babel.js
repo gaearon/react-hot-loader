3a2b65964cd8da4d3fadbbcaa33179207b70545b
Greg Bergé <berge.greg@gmail.com>
2017-12-23 14:37:44 +0100

    tests: add boilerplate

---
+++ boilerplate/webpack.config.babel.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: ['../packages/react-hot-loader/patch', './src/index'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.js$/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin()],
+}
--- packages/react-hot-boilerplate/src/App.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class App extends Component {
-  render() {
-    return (
-      <h1>Hello, world.</h1>
-    );
-  }
-}
