f29764f4c758f91d3f62f31f88548d2143a8e2c5
Greg Bergé <berge.greg@gmail.com>
2017-12-25 11:51:06 +0100

    docs: add decorators example

---
+++ examples/decorators/webpack.config.babel.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: ['./src/index'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules|packages/,
+        test: /\.js$/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin(), new webpack.NamedModulesPlugin()],
+}
