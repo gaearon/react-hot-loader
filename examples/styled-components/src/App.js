8e25a76a9786913021cea37f20af4cd812fd3175
Anton Korzunov <thekashey@gmail.com>
2018-01-19 22:30:42 +1100

    suppress warnings, #804

--- examples/styled-components/src/App.js
+++ examples/styled-components/src/App.js
@@ -1,4 +1,4 @@
-import { hot } from 'react-hot-loader'
+import { hot, setConfig } from 'react-hot-loader'
 import * as React from 'react'
 import styled from 'styled-components'
 import emoStyled from 'react-emotion'
@@ -12,14 +12,27 @@ const SmallText = emoStyled('div')`
   font-size: 22px;
 `
 
+const indirect = {
+  element: () => (
+    <SmallText>
+      hidden2 <Counter />
+    </SmallText>
+  ),
+}
+
+const aNumber = 10
+
 const App = () => (
   <h1>
-    <BigText>Hello, world.</BigText>
+    <BigText>1.Hello, world!! {aNumber} </BigText>
     <br />
-    <SmallText>Hello, world.</SmallText>
+    <SmallText>2.Hello, world---.</SmallText>
     <br />
     <Counter />
+    <indirect.element />
   </h1>
 )
+
+setConfig({ logLevel: 'debug' })
 
 export default hot(module)(App)
