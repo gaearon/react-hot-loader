0335365efc4a6695a85d9dc0799d4946dae29cfd
Anton Korzunov <thekashey@gmail.com>
2018-01-22 17:37:47 +1100

    dont use broken 'module'. Fixes #812

--- packages/react-hot-loader/src/global/modules.js
+++ packages/react-hot-loader/src/global/modules.js
@@ -1,12 +1,22 @@
+import logger from '../logger'
+
 const openedModules = {}
 
-export const isOpened = sourceModule => !!openedModules[sourceModule.id]
+export const isOpened = sourceModule =>
+  sourceModule && !!openedModules[sourceModule.id]
 
 export const enter = sourceModule => {
-  const moduleId = sourceModule.id
-  openedModules[moduleId] = true
+  if (sourceModule && sourceModule.id) {
+    openedModules[sourceModule.id] = true
+  } else {
+    logger.warn(
+      'React-hot-loader: no `module` variable found. Do you shadow system variable?',
+    )
+  }
 }
 
 export const leave = sourceModule => {
-  delete openedModules[sourceModule.id]
+  if (sourceModule && sourceModule.id) {
+    delete openedModules[sourceModule.id]
+  }
 }
