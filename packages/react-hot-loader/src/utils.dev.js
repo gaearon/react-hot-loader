b51f1c18311fcfd91f11a15769c66540bba80b25
Anton Korzunov <thekashey@gmail.com>
2018-01-25 23:26:25 +1100

    break cyclic dependency. fixes #820

--- packages/react-hot-loader/src/utils.dev.js
+++ packages/react-hot-loader/src/utils.dev.js
@@ -1,9 +1,11 @@
 import { getProxyByType } from './reconciler/proxies'
 import reactHotLoader from './reactHotLoader'
+import logger from './logger'
+import { setConfig as setProxyConfig } from 'react-stand-in'
+
+setProxyConfig({ logger })
 
 export const areComponentsEqual = (a, b) =>
   getProxyByType(a) === getProxyByType(b)
 
-export const setConfig = config => {
-  Object.assign(reactHotLoader.config, config)
-}
+export const setConfig = config => Object.assign(reactHotLoader.config, config)
