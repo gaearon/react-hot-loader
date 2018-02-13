31fb50fb4ed59c943bde3372c7f4cda583ec2877
Anton Korzunov <thekashey@gmail.com>
2018-02-13 19:49:26 +1100

    add SSR+HRM example

---
+++ examples/SSR/webpack.config.babel.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: ['webpack/hot/dev-server', './src/index'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        //exclude: /node_modules|packages/,  // should work without exclude
+        test: /\.js$/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+}
