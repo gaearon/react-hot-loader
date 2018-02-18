de9864fc4b41d1a226fcb917d308016626b293f2
Anton Korzunov <thekashey@gmail.com>
2018-02-19 08:18:45 +1100

    disable RHL in production, and when HMR not enabled

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-if (process.env.NODE_ENV === 'production') {
+if (!module.hot || process.env.NODE_ENV === 'production') {
   module.exports = require('./dist/react-hot-loader.production.min.js');
 } else {
   module.exports = require('./dist/react-hot-loader.development.js');
