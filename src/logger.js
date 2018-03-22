87ad586a01d76585eba492d27f235a66650acb2b
nulltoken <emeric.fermas@gmail.com>
2018-03-22 17:46:44 +0100

    Make IE11 consumers life (a little bit) easier
    Similarly to #870, provides a pure IE11 compliant includes-like for users that don't polyfill.

--- src/logger.js
+++ src/logger.js
@@ -3,22 +3,22 @@ import configuration from './configuration'
 
 const logger = {
   debug(...args) {
-    if (['debug'].includes(configuration.logLevel)) {
+    if (['debug'].indexOf(configuration.logLevel) !== -1) {
       console.debug(...args)
     }
   },
   log(...args) {
-    if (['debug', 'log'].includes(configuration.logLevel)) {
+    if (['debug', 'log'].indexOf(configuration.logLevel) !== -1) {
       console.log(...args)
     }
   },
   warn(...args) {
-    if (['debug', 'log', 'warn'].includes(configuration.logLevel)) {
+    if (['debug', 'log', 'warn'].indexOf(configuration.logLevel) !== -1) {
       console.warn(...args)
     }
   },
   error(...args) {
-    if (['debug', 'log', 'warn', 'error'].includes(configuration.logLevel)) {
+    if (['debug', 'log', 'warn', 'error'].indexOf(configuration.logLevel) !== -1) {
       console.error(...args)
     }
   },
