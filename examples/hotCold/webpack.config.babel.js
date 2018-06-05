586dd786d55e1a038f91bdd96738b9f4062dd036
Anton Korzunov <thekashey@gmail.com>
2018-06-05 23:30:03 +1000

    cold/parcel prerelease fixes

--- examples/hotCold/webpack.config.babel.js
+++ examples/hotCold/webpack.config.babel.js
@@ -3,6 +3,11 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const babelPlugin = path.resolve(
+  __dirname,
+  './node_modules/react-hot-loader/babel',
+)
+
 module.exports = {
   entry: ['./src/index'],
   output: {
@@ -12,16 +17,32 @@ module.exports = {
   module: {
     rules: [
       {
-        //exclude: /node_modules|packages/,  // should work without exclude
+        exclude: /node_modules|packages/,
         test: /\.js$/,
         use: 'babel-loader',
+      },
+      {
+        include: /node_modules/,
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // plugins: ['react-hot-loader/babel'] // <<----- you need this
+
+            plugins: [babelPlugin], // you DON'T need this
+          },
+        },
       },
     ],
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
     alias: {
+      // you don't need this
       react: path.resolve(path.join(__dirname, './node_modules/react')),
+      'react-hot-loader': path.resolve(
+        path.join(__dirname, './node_modules/react-hot-loader'),
+      ),
       'babel-core': path.resolve(
         path.join(__dirname, './node_modules/@babel/core'),
       ),
