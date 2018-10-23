308f35ea709b5871c073873fa27570059fbe127a
Sergey Melyukov <s.melukov@gmail.com>
2018-10-24 01:10:42 +0300

    drop `hot` calls for prod mode closes #1080

--- src/babel.prod.js
+++ src/babel.prod.js
@@ -1,3 +1,64 @@
+const RHLPackage = 'react-hot-loader'
+
+function isImportFromRHL(path, name) {
+  const binding = path.scope.getBinding(name)
+  const bindingType = binding && binding.path.node.type
+
+  if (
+    bindingType === 'ImportSpecifier' ||
+    bindingType === 'ImportDefaultSpecifier' ||
+    bindingType === 'ImportNamespaceSpecifier'
+  ) {
+    const bindingParent = binding.path.parent
+    return bindingParent.source.value === RHLPackage
+  }
+
+  return false
+}
+
+function isRHLContext(file) {
+  const { modules } = file.metadata
+
+  if (modules && Array.isArray(modules.imports)) {
+    const { imports } = modules
+
+    for (let i = 0; i < imports.length; i++) {
+      const { source } = imports[i]
+
+      if (source === RHLPackage) {
+        return true
+      }
+    }
+  }
+
+  return false
+}
+
 export default function plugin() {
-  return { visitor: {} }
+  function handleCall(path) {
+    if (!this.cancel) {
+      if (
+        path.node.callee.name === 'hot' &&
+        // ensure that this is `hot` from RHL
+        isImportFromRHL(path, 'hot') &&
+        path.parent.type === 'CallExpression' &&
+        path.parent.arguments[0] &&
+        path.parent.arguments[0].type === 'Identifier'
+      ) {
+        path.parentPath.replaceWith(path.parent.arguments[0])
+      }
+    }
+  }
+
+  return {
+    pre() {
+      // ignore files that do not use RHL
+      if (!isRHLContext(this.file)) {
+        this.cancel = true
+      }
+    },
+    visitor: {
+      CallExpression: handleCall,
+    },
+  }
 }
---
+++ test/__babel_fixtures__/drop-hot.prod.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+const App = () => <div>Hello World!</div>
+
+export default hot(module)(App)
