987fd6bf733fdee334d491d3b3baa61769484a21
ahmed <ahmedkorrim@gmail.com>
2019-05-10 01:05:42 +0200

    updated webpack-modern example * implemented hot dom patcher * updated babel & webpack

--- examples/webpack-modern/webpack.config.babel.js
+++ examples/webpack-modern/webpack.config.babel.js
@@ -10,6 +10,11 @@ module.exports = {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  resolve: {
+    alias: {
+        'react-dom': path.resolve( path.join( __dirname, '.', 'node_modules', '@hot-loader/react-dom' ) )
+    }
+  },
   module: {
     rules: [
       {
