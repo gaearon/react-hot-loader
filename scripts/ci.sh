b67872626cb56655a09e527841472b748d3c926f
Anton Korzunov <thekashey@gmail.com>
2020-03-06 10:35:56 +1100

    chore: separate tests for 15/16.10/16-latest

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -23,16 +23,32 @@ yarn test:react-dom:prepare
 # echo "\n\n"
 # echo "\n\n"
 
-echo "Installing React 16"
+echo "Installing React 16.10"
+yarn add react@16 react-dom@16.10 react-test-renderer@16.10 --pure-lockfile
+echo "\n\n"
+
+yarn test:react-dom:prepare
+
+echo "Running tests on React 16.10 - Babel ES2015"
+yarn test:es2015
+echo "\n\n"
+
+echo "Running tests on React 16.10 - Babel Modern"
+yarn test:modern --coverage && codecov
+echo "\n\n"
+
+
+
+echo "Installing React 16:latest"
 yarn add react@16 react-dom@16 react-test-renderer@16 --pure-lockfile
 echo "\n\n"
 
 yarn test:react-dom:prepare
 
-echo "Running tests on React 16 - Babel ES2015"
+echo "Running tests on React 16:latest - Babel ES2015"
 yarn test:es2015
 echo "\n\n"
 
-echo "Running tests on React 16 - Babel Modern"
+echo "Running tests on React 16:latest - Babel Modern"
 yarn test:modern --coverage && codecov
 echo "\n\n"
