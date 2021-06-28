5e1619099f550a73b4af169d9ca0e5304771c9b8
Meaghan Harty <1829246+meagonqz@users.noreply.github.com>
2021-06-28 12:28:39 -0400

    Babel plugin: support using BABEL_ENV=production

--- babel.js
+++ babel.js
@@ -1,6 +1,6 @@
 'use strict';
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production' || process.env.BABEL_ENV === 'production') {
   module.exports = require('./dist/babel.production.min.js');
 } else {
   module.exports = require('./dist/babel.development.js');
